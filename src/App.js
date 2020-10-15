67e947cc98982d552b8543ed4579feb166dd059c
ninohero <ninohero@muzocalo.com>
2020-10-15 16:17:35 -0400

    more hooks and effects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    getResults();
+  }, [])
+
+  const getResults = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios
+        .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err)
+    }
+    setLoading(false);
+  }
+
+  const handleClear = () => {
+    setQuery('');
+    searchInputRef.current.focus()
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getResults();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(event) => {setQuery(event.target.value)}}
+        value={query}
+        ref={searchInputRef} />
+      <button type="submit">Search</button>
+      <button type="button" onClick={() => handleClear()}>Clear</button>
+    </form>
+    {loading ? (
+      <div>Loading</div>
+    ) : (
+        <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+      )
+    }
+
+    {error && <div>{error.message}</div>}
+    </>
+  )
 }
 
 export default App;
